403d8e92e2b95368f534a79a04abd5fbfb647324
lachmanfrgymbos <frantisek.lachman.2023@gymbos.net>
2026-05-18 14:55:10 +0200

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -381,3 +381,20 @@ async function loadVisits() {
 }
 
 loadVisits();
+
+async function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert('Špatné přihlášení');
+    return;
+  }
+
+  window.location.href = 'admin.html';
+}

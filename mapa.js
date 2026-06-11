cae47da1cef40f744a217f4fa0b71b5bead93989
lachmanfrgymbos <frantisek.lachman.2023@gymbos.net>
2026-06-11 21:59:02 +0200

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,1 +1,23 @@
+function switchFloor(floor) {
+  const map = document.getElementById("map");
+  const text = document.getElementById("map-text");
 
+  if (floor === "1") {
+    text.innerText = "1. patro - mapa se načte později";
+  }
+
+  if (floor === "2") {
+    text.innerText = "2. patro - mapa se načte později";
+  }
+
+  if (floor === "3") {
+    text.innerText = "3. patro - mapa se načte později";
+  }
+
+  if (floor === "dvorek") {
+    text.innerText = "Altán / Dvorek - mapa se načte později";
+  }
+
+  // tady později:
+  // vyměníme background image + klikací stánky
+}

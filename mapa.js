e87254ca28fc72d5b9f5e011749e878bb2dafaa5
lachmanfrgymbos <frantisek.lachman.2023@gymbos.net>
2026-06-12 13:41:43 +0200

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,28 +1,21 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
+document.addEventListener("DOMContentLoaded", () => {
 
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
+  const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
 });
 
 function switchFloor(floor) {
 
   const text = document.getElementById("map-text");
 
-  if (floor === "1") {
-    text.innerText = "1. patro - mapa se načte později";
-  }
-
-  if (floor === "2") {
-    text.innerText = "2. patro - mapa se načte později";
-  }
-
-  if (floor === "3") {
-    text.innerText = "3. patro - mapa se načte později";
-  }
-
-  if (floor === "dvorek") {
-    text.innerText = "Altán / Dvorek - mapa se načte později";
-  }
+  if (floor === "1") text.innerText = "1. patro - mapa se načte později";
+  if (floor === "2") text.innerText = "2. patro - mapa se načte později";
+  if (floor === "3") text.innerText = "3. patro - mapa se načte později";
+  if (floor === "dvorek") text.innerText = "Altán / Dvorek - mapa se načte později";
 
 }
